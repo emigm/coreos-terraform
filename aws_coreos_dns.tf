20cbe7dc9a23e20a459641b3ccaa04794d7a420d
Emiliano Molina <emiliano.g.molina@gmail.com>
2015-08-28 16:39:21 -0300

    Update aws_coreos_dns.tf

--- aws_coreos_dns.tf
+++ aws_coreos_dns.tf
@@ -5,7 +5,7 @@
 resource "aws_route53_record" "docker_registry_rec" {
     #zone_id = "${aws_route53_zone.coreos_hosted_zone.zone_id}"
     zone_id = "${var.r53_hosted_zone_id}"
-    name = "docker-registry.fsws.infra-host.com"
+    name = "docker-registry.${var.r53_hosted_zone_name}"
     records = [
         "${aws_instance.coreos_docker_registry.public_ip}"
     ]
